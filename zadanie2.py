318945bfae292ea6a0447472da9f3a305e2a3667
d3borat <117433508+d3borat@users.noreply.github.com>
2023-06-25 16:52:06 +0200

    Add files via upload

---
+++ zadanie2.py
@@ -0,0 +1,6 @@
+import random
+try:
+    ileliczb = int(input("Podaj liczbę typowanych liczb: "))
+    maksliczba = int(input("Podaj maksymalną losowaną liczbę: "))
+    if ileliczb > maksliczba:
+        exit()
